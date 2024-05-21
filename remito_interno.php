663d7752930c9d0cdad8cf7d2f9ba386889a00ce
hernanx32 <webfsa@gmail.com>
2024-05-21 10:06:32 -0300

    asdasdasd

--- remito_interno.php
+++ remito_interno.php
@@ -34,20 +34,20 @@ $sql=$conn->query("SELECT * FROM `usuario` WHERE id_usuario ='$id_us' ");
   <tbody>
     <tr>
       <th colspan="5" scope="col">Buscar Remito</th>
-      <th width="214" colspan="4" rowspan="3" scope="col"><a href="remito_interno_carga.php">Agregar Remito</a></th>
+      <th width="221" colspan="4" scope="col"><a class="btn btn-success btn-lg" href="remito_interno_carga.php">Agregar Remito</a></th>
       </tr>
     <tr>
-      <td width="84" bgcolor="#A6A6A6"><label for="NroRem:">Nro. Rem:</label></td>
-      <td width="144" bgcolor="#A6A6A6"><input type="text" name="NroRem:" id="NroRem:"></td>
-      <td width="67" bgcolor="#A6A6A6"><label for="Origen">Origen:</label></td>
-      <td width="169" bgcolor="#A6A6A6"><input type="text" name="Origen" id="Origen"></td>
-      <td width="72" rowspan="2" bgcolor="#A6A6A6"><input type="button" name="button" id="button" value="Buscar"></td>
+      <td width="78" bgcolor="#A6A6A6"><label for="NroRem:">Nro. Rem:</label></td>
+      <td width="125" bgcolor="#A6A6A6"><label for="Origen2">Origen:</label></td>
+      <td width="162" bgcolor="#A6A6A6"><label for="destino2">Destino:</label></td>
+      <td width="169" bgcolor="#A6A6A6">Fecha:</td>
+      <td colspan="5" rowspan="2" bgcolor="#A6A6A6"><input type="button"  class="btn btn-secondary" name="button" id="button" value="Buscar"></td>
       </tr>
     <tr>
-      <td bgcolor="#A6A6A6"><label for="fecha">Fecha:</label></td>
+      <td bgcolor="#A6A6A6"><input name="NroRem:" type="text" id="NroRem:" size="10" maxlength="10"></td>
+      <td bgcolor="#A6A6A6"><input name="Origen" type="text" id="Origen" size="10" maxlength="15"></td>
+      <td bgcolor="#A6A6A6"><input name="destino" type="text" id="destino" size="10" maxlength="15"></td>
       <td bgcolor="#A6A6A6"><input name="fecha" type="text" id="fecha" placeholder="<?PHP echo date('d-m-Y');?>"></td>
-      <td bgcolor="#A6A6A6"><label for="destino">Destino:</label></td>
-      <td bgcolor="#A6A6A6"><input type="text" name="destino" id="destino"></td>
       </tr>
   </tbody>
 </table>
--- remito_interno_carga.php
+++ remito_interno_carga.php
@@ -29,33 +29,9 @@ $sql=$conn->query("SELECT * FROM `usuario` WHERE id_usuario ='$id_us' ");
 ?>
 
 <form action="/Sisfage/remito_interno_carga.php?scr=carga" method="post" name="form1" id="form1" accept-charset="UTF-8">
-   
-<table width="790" border="1" align="center">
-  <tbody>
-    <tr>
-      <th colspan="5" scope="col">Ingresar Remito Remito</th>
-      <th width="214" colspan="4" rowspan="3" scope="col">&nbsp;</th>
-      </tr>
-    <tr>
-      <td width="84" bgcolor="#A6A6A6"><label for="NroRem:">Nro. Rem:</label></td>
-      <td width="144" bgcolor="#A6A6A6"><input type="text" name="NroRem:" id="NroRem:"></td>
-      <td width="67" bgcolor="#A6A6A6"><label for="Origen">Origen:</label></td>
-      <td width="169" bgcolor="#A6A6A6"><input type="text" name="Origen" id="Origen"></td>
-      <td width="72" rowspan="2" bgcolor="#A6A6A6"><input type="button" name="button" id="button" value="Buscar"></td>
-      </tr>
-    <tr>
-      <td bgcolor="#A6A6A6"><label for="fecha">Fecha:</label></td>
-      <td bgcolor="#A6A6A6"><input name="fecha" type="text" id="fecha" placeholder="<?PHP echo date('d-m-Y');?>"></td>
-      <td bgcolor="#A6A6A6"><label for="destino">Destino:</label></td>
-      <td bgcolor="#A6A6A6"><input type="text" name="destino" id="destino"></td>
-      </tr>
-  </tbody>
-</table>
 </form>
 <?php
 $focus='d_nombre';
 $conn->close();
-
 pieprincipal($focus,$path);
-
 ?>
