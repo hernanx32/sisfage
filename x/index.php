058f0a297d5fb30a4f5d7d2e018ddbf553a2c4c5
hernanx32 <webfsa@gmail.com>
2024-12-30 12:14:08 -0300

    asdasdasd

--- x/index.php
+++ x/index.php
@@ -3,10 +3,11 @@ include("../Modulos/conex.php");
 ?>
 
 <!doctype html>
-<html>
+<html lang='es'>
 <head>
 <meta charset="utf-8">
-<title>Documento sin título</title>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+	<title>Documento sin título</title>
 </head>
 
 <body>
@@ -18,8 +19,24 @@ include("../Modulos/conex.php");
     <input type="search" name="buscar" id="buscar">
     <input name="btnBuscar" type="button" id="btnBuscar" onclick="abrirVentanaBusqueda()" value="Buscar">
   </p>
-  
-	
+ 
+    <script>
+        // JavaScript maneja los atajos de teclado
+        document.addEventListener('keydown', function(event) {
+            if (event.ctrlKey && event.key === 's') {
+                event.preventDefault(); // Evita la acción predeterminada del navegador
+                alert('¡Atajo Ctrl+S activado!');
+            } else if (event.altKey && event.key === 'a') {
+                alert('¡Atajo Alt+A activado!');
+            }
+        });
+    </script>
+</head>
+<body>
+    <h1>Ejemplo de atajos de teclado</h1>
+    <p>Pulsa <b>Ctrl+S</b> o <b>Alt+A</b> para activar un atajo.</p>
+</body>
+</html>	
 	
 
 	<script>
