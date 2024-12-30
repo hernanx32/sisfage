5ba0bafd1dd9a71b85484e762d2d0bd4bebebf72
hernanx32 <webfsa@gmail.com>
2024-12-30 12:36:15 -0300

    asdasd

---
+++ testing/Busqueda de Articulos/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+<form action="index.php" id="form1" name="form1" method="post">
+<table width="500" border="0" align="center" summary="Buscador de Artiulos Avanzado para facturador y remitos">
+  <caption>
+    Buscador Articulo
+  </caption>
+  <tbody>
+    <tr>
+      <td width="156"><input name="BuscaArt" type="search" id="BuscaArt" tabindex="1" title="Buscar Articulo" size="30" maxlength="30"></td>
+      <td width="162"><input name="btnBuscarArt" type=submit id="btnBuscarArt" form="form1" value="Buscar Articulo (F2)"></td>
+      <td width="160">&nbsp;</td>
+    </tr>
+    <tr>
+      <td>Detalle Articulo</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+  </tbody>
+</table>
+
+</form>
+<script>
+	document.form1.BuscaArt.focus();
+	
+	
+	</script>	
+</body>
+</html>
