2ae0d27ed4bffcf93969ad55f141defe08815828
hernanx32 <webfsa@gmail.com>
2024-12-31 16:50:06 -0300

    202431120450

---
+++ testing/DataTables/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+
+<link href="DataTables/datatables.min.css" rel="stylesheet">
+<script src="DataTables/datatables.min.js"></script>
+	
+	</head>
+
+<body>
+</body>
+</html>
