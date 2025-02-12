79f4ece4484748e9f47dd385d8af05cb45ae54ba
unknown <Hernan@PC-Hernan.apa.corp>
2025-02-11 21:02:41 -0300

    123132

---
+++ testing/Busc_ventana_emergente/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+<form action="index.php" method="get" name="Buscar" id="Buscar">
+  <input name="busc_art" type="search" autofocus="autofocus" id="busc_art" form="Buscar" placeholder="Buscar Articulo" tabindex="1" autocomplete="off" size="20">
+	Cant.: <input name="cant." type="number" id="cant." max="1000" min="1" tabindex="2" value="1" >
+</form>
+
+<br>
+<form action="index.php" method="get" name="agregarArt" id="Buscar">
+  <input name="art" type="search" autofocus="autofocus" id="art" form="Buscar" placeholder="Buscar Articulo" tabindex="1" autocomplete="off" size="20">
+</form>	
+	
+	
+	
+	<script>
+document.Buscar.busc_art.focus();
+	
+	</script>
+</body>
+</html>
